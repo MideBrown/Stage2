f0017c869dc8f7fca8b297d536efcc4515307743
Brown Olumide <mideplugs@gmail.com>
2021-08-18 14:27:43 +0100

    initial commit

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-console.log('Brown Olumide')
+var X = "BROWN OLUMIDE";
+
+window.alert('BROWN OLUMIDE')
+
+document.getElementById('foot01').innerHTML = "<p>&copy; " + new Date ().getFullYear () + "HNGi8. all rights reserved </p>";
